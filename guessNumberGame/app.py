9c8778367a19b942a292b195e2c31e8f9120981a
ZoDSeR97 <30656356+ZoDSeR97@users.noreply.github.com>
2023-03-10 18:23:55 -0500

    refactoring the code

--- guessNumberGame/app.py
+++ guessNumberGame/app.py
@@ -7,37 +7,27 @@ app.secret_key = secrets.token_hex()
 
 @app.route('/')
 def index():
-    gotIt = False
-    hint = ""
-    color = "bg-danger"
+    # Initialize secret number
     if 'num' not in session:
         session['num'] = randint(1, 100)
-    if "guess" not in session:
-        session['guess'] = 0
-    if "res" not in session:
-        session['res'] = False
+    
+    # Initialize attempts
     if "att" not in session:
         session['att'] = 1
-    elif session['att'] < 5:
-        if int(session['guess']) < session['num']:
-            hint = "Too Low!"
-        elif int(session['guess']) > session['num']: 
-            hint = "Too High!"
-        else:
-            hint = str(session['num'])+" was the number, took "+str(session['att'])+" attempts"
-            color = "bg-success"
-            gotIt = True
-        session['att']+=1
-    else:
-        gotIt = True
-        hint = "You Lose!"
-    return render_template("index.html", gotIt=gotIt, hint=hint, color=color)
+    
+    # Initialize Game State
+    if "gameOver" not in session:
+        session["gameOver"] = False
+    
+    return render_template("index.html")
 
 
 @app.route('/process', methods=['POST'])
 def process():
-    session['guess'] = request.form['guessNum']
-    session['res'] = True
+    if request.form['guessNum'].isdigit():
+        session['guess'] = int(request.form['guessNum'])
+        session['att']+=1
+        session['gameOver'] = session['guess']==session['num'] or session['att'] == 6
     return redirect('/')
 
 @app.route('/clear')
